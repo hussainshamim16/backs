4675aa46de55761e2b6e852eab8aaee9de55eec8
hussainshamim16 <hj422748@gmail.com>
2025-01-27 15:30:46 +0500

    ok

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';  
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 import UserRoutes from './routes/user.routes.js'
 
 app.get('/', (req, res) => {
